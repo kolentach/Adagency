c003eee9a0b34549129a2d72640876f29910290a
kolentach <34936732+kolentach@users.noreply.github.com>
2018-05-16 14:51:37 +0300

    Add files via upload

---
+++ WebApp/Global.asax.cs
@@ -0,0 +1,28 @@
+﻿using Ninject;
+using Ninject.Modules;
+using Ninject.Web.Mvc;
+using BL.Infrastructure;
+using NLayerApp.WEB.Util;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace WebApp
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            NinjectModule orderModule = new OrderModule();
+            NinjectModule serviceModule = new ServiceModule("DefaultConnection");
+            var kernel = new StandardKernel(orderModule, serviceModule);
+            kernel.Unbind<ModelValidatorProvider>();
+            DependencyResolver.SetResolver(new NinjectDependencyResolver(kernel));
+        }
+    }
+}
---
+++ WebApp/Util/OrderModule.cs
@@ -0,0 +1,14 @@
+﻿using Ninject.Modules;
+using BL.Services;
+using BL.Interfaces;
+
+namespace NLayerApp.WEB.Util
+{
+    public class OrderModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IOrderService>().To<OrderService>();
+        }
+    }
+}
