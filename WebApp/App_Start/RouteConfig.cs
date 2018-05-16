5cd311e600d59b2c62992920616484d765294c61
kolentach <34936732+kolentach@users.noreply.github.com>
2018-05-16 14:55:47 +0300

    Add files via upload

---
+++ WebApp/App_Start/RouteConfig.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApp
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Index",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Login",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Signup",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Account", action = "Signup", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Logout",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Account", action = "Logout", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
