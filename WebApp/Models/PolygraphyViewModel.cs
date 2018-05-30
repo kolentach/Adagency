5e182c8170f83ed7cd3888f57634cffa7cc0ebd0
kolentach <34936732+kolentach@users.noreply.github.com>
2018-05-30 14:05:16 +0300

    Add files via upload

---
+++ WebApp/Models/PolygraphyViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Web.Models
+{
+    public class PolygraphyViewModel
+    {
+        public int ID { get; set; }
+        public decimal Sum { get; set; }
+        public int Amount { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public int AdvertisingID { get; set; }
+    }
+}
