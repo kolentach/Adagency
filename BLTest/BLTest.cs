486925873c9eb962cf3c005d247b99f4096ab00d
kolentach <34936732+kolentach@users.noreply.github.com>
2018-05-30 13:52:57 +0300

    Add files via upload

---
+++ BLTest/BLTest.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using NUnit.Framework;
+using Moq;
+using BL.Infrastructure;
+using BL.DTO;
+using Data.Interfaces;
+using Data.Entities;
+using BL.Interfaces;
+using BL.Services;
+using Data.EF;
+using Data.Repositories;
+
+namespace BLTest
+{
+    [TestFixture]
+    public class BLTest
+    {
+        IOrderService serv;
+
+        [Test]
+        public void TestMakeOrder()
+        {
+            var uow = new EFUnitOfWork("defaultConnection");
+            serv = new OrderService(uow);
+            OrderDTO order = new OrderDTO { ID = 1, Date = DateTime.Now, AdvertisingID = 1, PhoneNumber = "12312321", Sum = 100 };
+            serv.MakeOrder(order);
+
+            uow.Orders.GetAll();
+            Order test = uow.Orders.GetAll().GetEnumerator().Current;
+
+            Assert.That(test.ID == order.ID);
+            Assert.That(test.AdvertisingID == order.AdvertisingID);
+            Assert.That(test.Date == order.Date);
+            Assert.That(test.PhoneNumber == order.PhoneNumber);
+        }
+
+        [Test]
+        public void TestMakePolygrahyOrder()
+        {
+            var uow = new EFUnitOfWork("defaultConnection");
+            serv = new OrderService(uow);
+            PolygraphyDTO order = new PolygraphyDTO { ID = 1, Date = DateTime.Now, AdvertisingID = 1, PhoneNumber = "12312321", Sum = 100, Amount = 20 };
+            serv.MakePolygrahyOrder(order);
+
+            uow.Orders.GetAll();
+            Order test = uow.Orders.GetAll().GetEnumerator().Current;
+
+            Assert.That(test.ID == order.ID);
+            Assert.That(test.AdvertisingID == order.AdvertisingID);
+            Assert.That(test.Date == order.Date);
+            Assert.That(test.PhoneNumber == order.PhoneNumber);
+            Assert.That(test.Amount == order.Amount);
+        }
+
+        [Test]
+        public void TestGetAds()
+        {
+            var uow = new EFUnitOfWork("defaultConnection");
+            serv = new OrderService(uow);
+            Advertising ad = new Advertising { ID = 1, Name = "Advertising", Description = "Description", Price = 100, Type = "type" };
+            uow.Ads.Create(ad);
+
+            AdvertisingDTO test = serv.GetAds().GetEnumerator().Current;
+
+            Assert.That(test.ID == ad.ID);
+            Assert.That(test.Name == ad.Name);
+            Assert.That(test.Price == ad.Price);
+            Assert.That(test.Type == ad.Type);
+            Assert.That(test.Description == ad.Description);
+        }
+
+        [Test]
+        public void TestGetAdvertising()
+        {
+            var uow = new EFUnitOfWork("defaultConnection");
+            serv = new OrderService(uow);
+            Advertising ad = new Advertising { ID = 1, Name = "Advertising", Description = "Description", Price = 100, Type = "type" };
+            uow.Ads.Create(ad);
+
+            AdvertisingDTO test = serv.GetAdvertising(ad.ID);
+
+            Assert.That(test.ID == ad.ID);
+            Assert.That(test.Name == ad.Name);
+            Assert.That(test.Price == ad.Price);
+            Assert.That(test.Type == ad.Type);
+            Assert.That(test.Description == ad.Description);
+        }
+
+        [Test]
+        public void TestEditAd()
+        {
+            var uow = new EFUnitOfWork("defaultConnection");
+            serv = new OrderService(uow);
+            Advertising ad = new Advertising { ID = 1, Name = "Advertising", Description = "Description", Price = 100, Type = "type" };
+            uow.Ads.Create(ad);
+
+            serv.EditAd(ad.ID, (new AdvertisingDTO { ID = 1, Name = "ChangedAdvertising", Description = "ChangedDescription", Price = 100, Type = "type" }));
+
+            AdvertisingDTO test = serv.GetAdvertising(ad.ID);
+            Assert.That(test.ID == ad.ID);
+            Assert.That(test.Name == ad.Name);
+            Assert.That(test.Price == ad.Price);
+            Assert.That(test.Type == ad.Type);
+            Assert.That(test.Description == ad.Description);
+        }
+
+        [Test]
+        public void TestFindAds()
+        {
+            var uow = new EFUnitOfWork("defaultConnection");
+            serv = new OrderService(uow);
+            Advertising ad = new Advertising { ID = 1, Name = "Advertising", Description = "Description", Price = 100, Type = "type" };
+            uow.Ads.Create(ad);
+
+            AdvertisingDTO test = serv.FindAds(ad.Name).GetEnumerator().Current;
+
+            Assert.That(test.ID == ad.ID);
+            Assert.That(test.Name == ad.Name);
+            Assert.That(test.Price == ad.Price);
+            Assert.That(test.Type == ad.Type);
+            Assert.That(test.Description == ad.Description);
+        }
+    }
+}
---
+++ BLTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BLTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BLTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9231e16d-3bbf-412e-be72-66b7031c4dce")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
